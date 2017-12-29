0b5ccfbbb6c1ac77623460022f81c32b754818f7
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-29 16:31:15 -0500

    ex6.rb update2

--- ex6.rb
+++ ex6.rb
@@ -3,5 +3,8 @@ puts "Enter tempurture in Fahrenheit?"
 fahrenheit = gets.to_i
 
 def conversion(tempurture)
-celsius = (fahrenheit - 32) x 5/9
+(tempurture - 32) * 5/9
 end
+
+
+puts "The tempurture is #{conversion(fahrenheit)} degrees celsius."
